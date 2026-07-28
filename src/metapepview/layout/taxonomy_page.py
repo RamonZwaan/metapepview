58cacd2ad9ef5178067a9620e9ba56538ce29f5a
Ramon van der Zwaan <ramonvanderzwa@tudelft.nl>
2026-07-28 16:26:39 +0200

    fixed missing callback id for figure data export

--- src/metapepview/layout/taxonomy_page.py
+++ src/metapepview/layout/taxonomy_page.py
@@ -363,7 +363,7 @@ taxonomy_barplot = [
         [
             html.H5("Figure", id="taxonomy_figure_title"),
             dbc.Button("Export figure data", 
-                       id="",
+                       id="export_taxonomy_figure_data",
                        className="me-3",
                        disabled=True)
         ],
